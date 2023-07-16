69eb5416baa8a4d5465dfac2c5c8b0b0eabba9cc
zubayermunna <muna700064@gmail.com>
2023-07-16 23:43:46 +0600

    added navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,13 @@
-import { Button, Typography, useTheme } from "@mui/material";
-import { SubNavBar,NavBar,DrawerComp } from "../components";
+
+import { SubNavBar,NavBar, } from "../components";
 
 const Home = () => {
-    const theme=useTheme()
+
     return (
       <div>
       <SubNavBar/>
       <NavBar/>
-      <DrawerComp/>
-      <Button variant="contained" sx={{color:theme.palette.font.primary,fontSize:theme.typography.headline}} >Home Button</Button>
-      <Typography variant="h1">Headline Text</Typography>
-      <Typography variant="h2">Subheadline Text</Typography>
-      <Typography variant="p">Paragraph Text</Typography>
+     
       </div>
     );
 };
