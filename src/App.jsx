9932bc08c95fc88151f9e7f3a1be92e692baae99
zubayermunna <muna700064@gmail.com>
2023-07-16 21:46:08 +0600

    material ui added and project environment setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <Routes>
+    <Routes>
         <Route path='/' element={<Home/>}></Route>
-      </Routes>
+    </Routes>
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 
 
 const Home = () => {
     return (
         <div>
             This is home
+            <Button variant="contained" >Home Button</Button>
         </div>
     );
 };
